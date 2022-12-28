6ec690f6271d66b19bcb214b311d4974671ef34f
kkobikim <kkobi.kim@gbike.io>
2022-12-29 00:21:45 +0900

    d

--- interfaces/Config.ts
+++ interfaces/Config.ts
@@ -1,9 +1,9 @@
 export interface Config {
   TOKEN: string;
-  PREFIX: string;
-  MAX_PLAYLIST_SIZE: number;
-  PRUNING: boolean;
-  STAY_TIME: number;
-  DEFAULT_VOLUME: number;
-  LOCALE: string;
+  PREFIX: "!";"
+  MAX_PLAYLIST_SIZE: 20;
+  PRUNING:  false;
+  STAY_TIME: 999999999;
+  DEFAULT_VOLUME: 100;
+  LOCALE: "ko";
 }
